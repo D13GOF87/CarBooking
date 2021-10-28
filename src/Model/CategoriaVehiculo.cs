46bb1da8eb8915dd7e9dff13c48262092ec8e060
Diego Salazar <65321048+D13GOF87@users.noreply.github.com>
2021-10-28 18:45:53 -0500

    Creación modelos CategoriaVehiculo y TipoVehiculo

---
+++ src/Model/CategoriaVehiculo.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+namespace Model
+{
+	public class CategoriaVehiculo
+	{
+		public int IdCategoriaVehiculo { get; set; }
+
+		public string NombreCategoria { get; set; }
+
+		public int EstadoCategoriaVehiculo { get; set; }
+
+		public ICollection<TipoVehiculo> TipoVehiculos { get; set; }
+	}
+}
---
+++ src/Model/TipoVehiculo.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Model
+{
+	public class TipoVehiculo
+	{
+		public int IdTipoVehiculo { get; set; }
+
+		public string NombreTipo { get; set; }
+
+		public int EstadoTipoVehiculo { get; set; }
+
+		public int IdCategoriaVehiculo { get; set; }
+
+		public CategoriaVehiculo CategoriaVehiculo { get; set; }
+	}
+}
