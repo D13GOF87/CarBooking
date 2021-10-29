0d9146b895e78b466b4c59c5c10a9b31ce8eec14
ian0000 <niklas0617@gmail.com>
2021-10-29 17:45:24 -0500

    colores y marca

---
+++ src/Model/ModeloColores.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Model
+{
+    class ModeloColores
+    {
+        public int IdColor { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string NombreColor { get; set; }
+s
+        //aqui va la conexion con vehiculos
+    }
+}
---
+++ src/Model/ModeloMarcas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Model
+{
+    class ModeloMarcas
+    {
+        public int IdMarcas { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string NombreMarca { get; set; }
+
+        //aqui va la conexion con vehiculos
+    }
+}
