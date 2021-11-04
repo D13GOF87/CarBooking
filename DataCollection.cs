d351dd145a3767502e72fd76d56274748cd44540
Diego Salazar <65321048+D13GOF87@users.noreply.github.com>
2021-11-04 12:25:57 -0500

    Paginacion

---
+++ DataCollection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public class DataCollection<T> where T : class 
+{
+	public IEnumerable<T> Items { get; set; }
+
+	//Cantidad de registros encontrados
+	public int Total { get; set; }
+
+	//Pagina actual que se está navegando
+	public int Page { get; set; }
+
+	//Cantidad de páginas en las que se pueden navegar
+	public int Pages { get; set; }
+}
---
+++ src/Service/Commons/DataCollection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Service.Commons
+{
+	public class DataCollection<T> where T : class
+	{
+		public bool HasItems
+		{
+			get
+			{
+				return Items != null && Items.Any();
+			}
+		}
+
+		public IEnumerable<T> Items { get; set; }
+
+		//Cantidad de registros encontrados
+		public int Total { get; set; }
+
+		//Pagina actual que se está navegando
+		public int Page { get; set; }
+
+		//Cantidad de páginas en las que se pueden navegar
+		public int Pages { get; set; }
+	}
+}
